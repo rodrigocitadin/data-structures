91af7c1d85e51dcac7ae0830e369d84245b696c1
rodrigocitadin <citadinrodrigo@gmail.com>
2023-03-02 15:06:45 -0300

    Queue structure

---
+++ src/queue.ts
@@ -0,0 +1,37 @@
+class Queue {
+  items: any[]
+
+  constructor() {
+    this.items = [];
+  }
+
+  push(item: any) {
+    this.items.push(item);
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  shift() {
+    return this.items.shift();
+  }
+
+  top() {
+    return this.items[this.size() - 1];
+  }
+
+  bottom() {
+    return this.items[0];
+  }
+
+  middle() {
+    if (!this.isEmpty) {
+      return this.items[Math.floor(this.size() / 2)]
+    }
+  }
+}
